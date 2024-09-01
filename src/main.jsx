eb2f4543cdf8bcb79ac183759a9e7faf62e0c84f
mafia0970 <126271223+mafia0970@users.noreply.github.com>
2024-09-01 19:30:10 +0530

    mainpage done

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.css";
 
-import ReactDOM from "react-dom/client";
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
+);
